ac4c36058141a69ad24e76c9a536b101b4d6fde3
rydnr <github@acm-sl.org>
2024-01-17 19:17:07 +0100

    Added vim and emacs settings

--- pythoneda/realm/unveilingpartner/application/unveilingpartner_app.py
+++ pythoneda/realm/unveilingpartner/application/unveilingpartner_app.py
@@ -57,3 +57,11 @@ if __name__ == "__main__":
     asyncio.run(
         UnveilingpartnerApp.main("pythoneda.realm.unveilingpartner.Unveilingpartner")
     )
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
